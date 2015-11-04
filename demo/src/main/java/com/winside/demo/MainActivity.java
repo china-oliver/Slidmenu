ee590adb356270e2dcb40f2a9d4306589b6cd54f
yupan <yupan>
2015-11-04 10:23:32 +0800

    Initial commit

---
+++ demo/src/main/java/com/winside/demo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.winside.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public <E extends View> E $(int id){
+        return (E)findViewById(id);
+    }
+}
---
+++ demo/src/main/java/com/winside/demo/utils/UserModel.java
@@ -0,0 +1,8 @@
+package com.winside.demo.utils;
+
+/**
+ * Created by JJ on 2015/11/2.
+ */
+public class UserModel {
+    
+}
